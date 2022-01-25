d410b86180b6c2a42c9c4e74f7bd663a12f484d2
18chanp1 <18chanp1@gmail.com>
2022-01-24 19:53:05 -0800

    renamed bonus binsearch

--- bonus/binsearch-bonus.asm
+++ bonus/binsearch-bonus.asm
@@ -1,6 +1,41 @@
 section .text
 
 global binsearch
-binsearch:
-    ret
 
+;1. lo-rdi, 2. hi-rsi, 3.search key - rdx;
+binsearch:
+cmp rsi, rdi
+jb notfound
+
+;Calculate mid;
+push rsi
+pop rax
+sub rax, rdi
+shr rax, 4
+sal rax, 3
+add rax, rdi
+
+
+
+cmp [rax],  rdx
+jne checkSmall
+ret
+
+checkSmall:
+jbe isLarge
+sub eax, 8
+push rax
+pop rsi
+call binsearch
+ret
+
+isLarge:
+add eax, 8
+push rax
+pop rdi
+call binsearch
+ret
+
+notfound:
+xor rax, rax
+ret
