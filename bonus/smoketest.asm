db82b55d5f5d1348a4378b2910c77dd2dd06b4e7
18chanp1 <18chanp1@gmail.com>
2022-01-24 19:45:31 -0800

    test and added bonus

---
+++ bonus/smoketest.asm
@@ -0,0 +1,44 @@
+STDOUT    equ 1
+SYS_write equ 1
+SYS_exit  equ 60
+
+extern binsearch
+
+section .text
+global _start
+_start:
+    mov  rdi, lo
+    mov  rsi, hi
+    mov  rdx, 9
+    call binsearch
+  r1:
+    cmp  rax, lo + 8*4
+    jne  failed
+    mov  rdi, lo
+    mov  rsi, hi
+    mov  rdx, 4
+    lbl:
+    call binsearch
+    cmp  rax, lo + 8*1
+    jne  failed
+    mov  rdx, plen
+    mov  rsi, pmsg
+    jmp  end
+failed:
+    mov  rdx, flen
+    mov  rsi, fmsg
+    jmp  end
+end:
+    mov  rax, SYS_write
+    mov  rdi, STDOUT
+    syscall
+    mov  rax, SYS_exit
+    mov  rdi, 0
+    syscall
+
+lo   dq 2, 4, 5, 6, 9, 13, 16, 17, 19
+hi   dq 20
+pmsg db "smoketest passed", 10
+plen equ $ - pmsg
+fmsg db "smoketest failed", 10
+flen equ $ - fmsg
